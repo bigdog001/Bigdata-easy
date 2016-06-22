73a3cfbb02fcad42e2d0d5159d5fcc4a830527aa
bigdog001 <bigdog001@users.noreply.github.com>
2016-06-22 13:51:15 -0400

    Update WordCount.java

--- Spark/src/main/java/com/bigdata/bigdog/spark/WordCount.java
+++ Spark/src/main/java/com/bigdata/bigdog/spark/WordCount.java
@@ -1,6 +1,15 @@
 package com.bigdata.bigdog.spark;
+import groovy.lang.Tuple;
+import java.util.Arrays;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
+import scala.Tuple2;
 public class WordCount{
     public static void main(String[] args) {
         
@@ -8,5 +17,38 @@ public class WordCount{
         SparkConf sconf = new SparkConf().setAppName("my spark").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(sconf);
         
+        JavaRDD<String> lines = sc.textFile("C:\\Users\\jw362j\\work_jiunian.wang\\Program_Files\\spark-1.6.0-bin-hadoop2.6\\README.md");
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String,String>(){
+
+            @Override
+            public Iterable<String> call(String line) throws Exception {
+               String [] words = line.split(" ");
+                return Arrays.asList(words);
+            }
+        });
+        JavaPairRDD<String,Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {           
+            @Override
+            public Tuple2<String, Integer> call(String word) throws Exception {
+                return new Tuple2<String, Integer>(word, 1);
+            }
+        });
+        
+       JavaPairRDD<String,Integer> wordsCount = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
+        
+        wordsCount.foreach(new VoidFunction<Tuple2<String, Integer>>() {
+
+            @Override
+            public void call(Tuple2<String, Integer> pairs) throws Exception {
+                System.out.println(""+pairs._1+":"+pairs._2);
+            }
+        });
+        
+        sc.close();
     }
 }
